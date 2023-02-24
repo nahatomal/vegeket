1a35b15c192ea93a633760ef60c302557ddd3ff5
nahatomal <naonao325@icloud.com>
2023-02-24 17:06:23 +0900

    first_comit

---
+++ base/models/item_models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField(default='', max_length=50)
+    price = models.PositiveIntegerField(default=0)
+    stock = models.PositiveIntegerField(default=0)
+    description = models.TextField(default='', blank=True)
+    sold_count = models.PositiveIntegerField(default=0)
+    is_published = models.BooleanField(default=False)
+    crated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
