59d8437242ec610fbd7f25e5885e24bda45b6729
nahatomal <naonao325@icloud.com>
2023-02-27 02:05:58 +0900

    setting admin

--- base/models/item_models.py
+++ base/models/item_models.py
@@ -1,7 +1,30 @@
 from django.db import models
+from django.utils.crypto import get_random_string
+import os
+def create_id():
+    return get_random_string(22)
 
+def upload_image_to(instance, filename):
+    item_id = instance.id
+    return os.path.join('static', 'items', item_id, filename)
 
+class Category(models.Model):
+    slug = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=32)
+    
+    def __str__(self):
+        return self.name
+    
+class Tag(models.Model):
+    slug = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=32)
+    
+    def __str__(self):
+        return self.name
+    
 class Item(models.Model):
+    id = models.CharField(default=create_id, primary_key=True, max_length=22, #ランダムな２２文字の文字列がIDになる
+    editable=False)
     name = models.CharField(default='', max_length=50)
     price = models.PositiveIntegerField(default=0)
     stock = models.PositiveIntegerField(default=0)
@@ -10,6 +33,11 @@ class Item(models.Model):
     is_published = models.BooleanField(default=False)
     crated_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(default="", blank=True,
+                              upload_to=upload_image_to)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True) #on_deleteは参照先が消えたときに何を割り当てるか
+    tags = models.ManyToManyField(Tag) #中間テーブルで間接的につながっている
     
     def __str__(self):
-        return self.name
+        return self.name + ' ID: ' + self.id
