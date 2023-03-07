c2f9ad42060f7ca9573995ee04f25bef650fe904
nahatomal <naonao325@icloud.com>
2023-03-08 01:44:00 +0900

    決済画面の実装

--- config/urls.py
+++ config/urls.py
@@ -16,17 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from base import views
-
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+ 
+    # Pay
+    path('pay/checkout/', views.PayWithStripe.as_view()),
+    path('pay/success/', views.PaySuccessView.as_view()),
+    path('pay/cancel/', views.PayCancelView.as_view()),
+ 
     # Cart
     path('cart/remove/<str:pk>/', views.remove_from_cart),
     path('cart/add/', views.AddCartView.as_view()),
-    path('cart/', views.CartListView.as_view()),
-    
+    path('cart/', views.CartListView.as_view()),  # カートページ
+ 
     # Items
     path('items/<str:pk>/', views.ItemDetailView.as_view()),
-    
-    path('', views.IndexListView.as_view()) # トップページ
+ 
+    path('', views.IndexListView.as_view()),  # トップページ
 ]
